8065a45c15312c3761e4801adda61df20ff03542
Daniel <brokenjava@gmail.com>
2018-09-10 19:53:37 -0700

    Add project files.

---
+++ SignatureScrapper/SignatureScrapper/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SignatureScrapper
+{
+    public partial class MainPage : ContentPage
+    {
+        //TODO uri builder to make next file
+        static string _uri = "http://www.loomisexpress.com/ca/SavedSigImg/";
+
+        UriImageSource _uriPath = new UriImageSource { Uri = new Uri(_uri) };
+        List<string> _urnList;
+        int _urnIndex = 0;
+
+    public MainPage()
+        {
+            InitializeComponent();
+            
+            _urnList = UriFileStream.FindBmpPaths(_uri);
+            updateImage();
+
+            //foreach (var item in _urnList)
+            //{
+            //    BmpStack.Children.Add(new Label() { Text = item.ToString() });
+            //}
+
+            //var textFromFile = new WebClient().DownloadString(_uri);
+            //StringEditor.Text = textFromFile;
+           
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+            //Actual_Width.Text = CurrentImage.Measure.Actual_Width   
+        }
+
+        private void OnNextButtonClicked(object sender, EventArgs e)
+        {
+            if (_urnIndex < _urnList.Count-1) _urnIndex++;
+            updateImage();
+        }
+
+        private void OnPrevButtonClicked(object sender, EventArgs e)
+        {
+            if (_urnIndex > 0) _urnIndex--;
+            updateImage();
+        }
+
+        private void updateImage()
+        {
+            var s = _urnList[_urnIndex];
+            var imageSource = new UriImageSource { Uri = new Uri("http://www.loomisexpress.com/ca/SavedSigImg/"+s) };
+
+            CurrentImage.Source = imageSource;
+
+            ImageString.Text = imageSource.ToString();
+            URIEntry.Text = imageSource.Uri.Segments.Last();
+        }   
+    }
+}
---
+++ SignatureScrapper/SignatureScrapper/UriFileStream.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace SignatureScrapper
+{
+    public static class UriFileStream
+    {
+        public static List<string> FindBmpPaths(string uriString)
+        {
+            List<string> urnList = new List<string>();
+
+            var client = new WebClient();
+            char[] buffer;
+
+            using (var stream = client.OpenRead(uriString))
+            using (var reader = new StreamReader(stream))
+            {
+
+                reader.ReadLine();
+                reader.ReadLine();
+
+                //(int)reader.BaseStream.Length
+                buffer = new char[1500];
+                reader.ReadBlock(buffer, 0, 1500);
+
+                //var text = reader.ReadLine();
+                var text = new string(buffer);
+                var splitText = text.Split('"', '/');
+
+                foreach (var item in splitText)
+                {
+                    if (item.Contains(".bmp") && !item.Contains(">")) urnList.Add(item);
+                }
+            }
+            return urnList;
+        }
+    }
+}
